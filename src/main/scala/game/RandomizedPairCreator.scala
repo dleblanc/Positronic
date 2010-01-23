6d4a89b8ebfec1ab14de4509d9063bfa8d515f97
Dave LeBlanc <david.leblanc@gmail.com>
2010-01-22 21:49:37 -0400

    Added another really crappy test, this time for actually inserting the apropriate number of matches.
    Looks ok, want to test with scalacheck.

--- src/main/scala/game/RandomizedPairCreator.scala
+++ src/main/scala/game/RandomizedPairCreator.scala
@@ -6,6 +6,18 @@ class RandomizedPairCreator {
 	
 	private[this] val randomGenerator = new Random()
 	
+	// @TailRec
+	def pickRandomElements[T](fromSequence: List[T], pairCount: Int): List[T] = {
+		if (pairCount == 0) {
+			return Nil
+		}
+		else {
+			val index = randomGenerator.nextInt(fromSequence.size)
+		
+			return fromSequence(index) :: pickRandomElements(fromSequence.slice(0, index) ::: fromSequence.slice(index + 1, fromSequence.length), pairCount - 1)
+		}
+	}
+	
 	def getRandomlyFromListExcludingElement[T <: Any](dontInclude: T, fromList: Array[T]):T = {
 		val values = fromList.filter(element => element != dontInclude)
 		return values(randomGenerator.nextInt(values.size))
@@ -23,6 +35,34 @@ class RandomizedPairCreator {
 			val shorterList = fillOutRandomlyWithNonMatchesNBack(listSize - 1, nBack, fromElements)
 			val nonMatch = getRandomlyFromListExcludingElement(shorterList(nBack - 1), fromElements)
 			nonMatch :: shorterList
+		}
+	}
+	
+	def insertNRandomMatches[T](originalList: List[T], nBack: Int, numberOfMatches: Int, fromCollection: List[T]): List[T] = {
+		val randomIndexes = pickRandomElements((0 until originalList.size - nBack).toList, numberOfMatches)
+		val sortedIndexes = randomIndexes sort((a, b) => a < b)
+		
+		introduceMatchesAtIndexes(originalList, sortedIndexes, fromCollection, 0, nBack)
+	}
+	
+	// @TailRec
+	def introduceMatchesAtIndexes[T](originalList: List[T], sortedIndexes: List[Int], fromCollection: List[T], currentIndex: Int, nBack: Int): List[T] = {
+		println("originalList: " + originalList)
+		println("sortedIndexes: " + sortedIndexes)
+		
+		if (sortedIndexes.size == 0) {
+			originalList
+		}
+		else if (sortedIndexes.head == currentIndex) {
+			// Shitballs, have to append to the start of this shit
+			
+			val smallerList = introduceMatchesAtIndexes((originalList.tail take nBack - 1) ::: List(originalList.head) ::: (originalList drop nBack + 1), sortedIndexes.tail, fromCollection, currentIndex + 1, nBack)
+			val result = originalList.head :: smallerList
+			println("combined to: " + result)
+			result
+		}
+		else {
+			originalList.head :: introduceMatchesAtIndexes(originalList.tail, sortedIndexes, fromCollection, currentIndex + 1, nBack) 
 		}
 	}
 }
--- src/test/scala/game/RandomizedPairCreatorTest.scala
+++ src/test/scala/game/RandomizedPairCreatorTest.scala
@@ -3,20 +3,21 @@ package game
 import org.scalatest.matchers._
 import org.scalatest._
 
+// BIG TODO: use scalacheck here, perfectly suited to this (no?)
 
 class RandomizedPairCreatorTest extends FunSuite with ShouldMatchers {
 	val creator = new RandomizedPairCreator()
 
 	// TODO: I don't think I need this any more
-//	test("Gets exaustive list of random elements returns all members") {
-//		val fromSequence = List("a", "b")
-//		val pairCount = 2
-//		
-//		val result = creator.pickRandomElements(fromSequence, pairCount)
-//		val sortedResult = result sort((a, b) => a < b)
-//		
-//		sortedResult should equal (List("a", "b"))
-//	}
+	test("Gets exaustive list of random elements returns all members") {
+		val fromSequence = List("a", "b")
+		val pairCount = 2
+		
+		val result = creator.pickRandomElements(fromSequence, pairCount)
+		val sortedResult = result sort((a, b) => a < b)
+		
+		sortedResult should equal (List("a", "b"))
+	}
 	
 	// I think I should test the pickRandomElements method more, but not sure how to.
 
@@ -43,7 +44,8 @@ class RandomizedPairCreatorTest extends FunSuite with ShouldMatchers {
 		 
 		 val testFunc = () => {
 			 val randomWithoutMatches = creator.fillOutRandomlyWithNonMatchesNBack(listSize, nBack, Array("a", "b", "c"))
-			 println(randomWithoutMatches)
+			 randomWithoutMatches.size should equal(listSize)
+			 
 			 for (i <- nBack until listSize) {
 				 if (randomWithoutMatches(i) == randomWithoutMatches(i - nBack)) {
 					 fail("index at i=" + i + " matches i=" + (i - nBack))
@@ -52,5 +54,16 @@ class RandomizedPairCreatorTest extends FunSuite with ShouldMatchers {
 		 }
 		 
 		 for (i <- 1 to 30) testFunc()
+	 }
+	 
+	 test("insertNRandomMatches puts two matches with nback=1 into the list") {
+		 val fromCollection = List("1", "2")
+		 val nonMatchingList = List("a", "b", "c")
+		 val nBack = 1
+		 val numberOfMatches = 2
+		 
+		 val result = creator.insertNRandomMatches(nonMatchingList, nBack, numberOfMatches, fromCollection)
+		 result(0) should equal (result(1))
+		 result(1) should equal (result(2))
 	 }
 }
